9873c158b4d66f16abe3c55d8632b6df64e5ea51
Jeanette Kgabe <jeanettekgabe@gmail.com>
2024-06-09 20:02:34 +0200

    first commit

---
+++ build/generated-sources/ap-source-output/za/ac/tut/entities/Student_.java
@@ -0,0 +1,18 @@
+package za.ac.tut.entities;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2024-06-09T18:55:23")
+@StaticMetamodel(Student.class)
+public class Student_ { 
+
+    public static volatile SingularAttribute<Student, Date> date;
+    public static volatile SingularAttribute<Student, byte[]> image;
+    public static volatile SingularAttribute<Student, String> surname;
+    public static volatile SingularAttribute<Student, String> name;
+    public static volatile SingularAttribute<Student, Long> id;
+
+}
---
+++ src/java/za/ac/tut/entities/Student.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.tut.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Jeanette
+ */
+@Entity
+public class Student implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    
+    private Long id;
+
+    @Lob
+    private byte[] image;
+    
+    private String name;
+    
+    private String surname;
+    
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date date;
+
+    public Student() {
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Student)) {
+            return false;
+        }
+        Student other = (Student) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "za.ac.tut.entities.Student[ id=" + id + " ]";
+    }
+    
+}
